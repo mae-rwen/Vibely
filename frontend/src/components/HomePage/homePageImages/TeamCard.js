c75897719b21be2158e1a2022ce537952a9df418
Agata Thrams <agata.thrams@gmail.com>
2023-02-16 14:19:02 +0100

    order in components changed

--- frontend/src/components/HomePage/homePageImages/TeamCard.js
+++ frontend/src/components/HomePage/homePageImages/TeamCard.js
@@ -3,9 +3,9 @@ export default function TeamCard({member}) {
   return (
     <>
     <div class="col-lg-3">
-      <img src={member} alt={member} style={{width:"140px", borderRadius: "50%"}}/>
-        <h3 class="fw-normal">{member}</h3>
-        <p>{member}</p>    
+      <img src={member.pic} alt={member.name} style={{width:"140px", borderRadius: "50%"}}/>
+        <h3 class="fw-normal">{member.name}</h3>
+        <p>{member.desc}</p>    
       </div>
     </>
   )
