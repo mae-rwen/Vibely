d40f024cdc0a315cd70bd9d39508d067a89d02cd
Aleksandra Marczewska <117514059+marczewska@users.noreply.github.com>
2023-02-20 23:36:53 +0100

    backend update

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ require("dotenv/config");
 require("./db");
 const express = require('express');
 const cors = require("cors");
+const corsOptions = require("./config/corsOptions");
 const cookieParser = require("cookie-parser")
 
 const { authRouter } = require("./routes/auth")
@@ -13,12 +14,11 @@ const { errorHandler } = require("./middlewares/errorHandler");
 const app = express();
 const port = process.env.PORT || 3001;
 
-// console.log(process.env.CONNECTION_STRING);
-
 app.use(cors({
-  // origin: ["http://localhost:300"],
-  // credentials: true,
-}));
+  origin: ["http://localhost:3000"],
+  credentials: true,
+  }
+));
 app.use(cookieParser());
 app.use(express.json());
 
