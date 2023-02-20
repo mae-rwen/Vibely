12109bd14a3704e32554879521a478e98238adb7
Agata Thrams <agata.thrams@gmail.com>
2023-02-20 14:00:42 +0100

    applying sass

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,9 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './styles/index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import "./App.css";
+import "./styles/styles.css";
 import App from './App';
 import { BrowserRouter } from "react-router-dom";
 
