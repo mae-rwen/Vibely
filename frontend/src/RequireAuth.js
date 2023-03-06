8b8509c5567d4f50984f6268b02e9e29e199aa64
Agata Thrams <agata.thrams@gmail.com>
2023-03-06 14:04:32 +0100

    delete of useless components

--- frontend/src/RequireAuth.js
+++ frontend/src/RequireAuth.js
@@ -13,7 +13,7 @@ const RequireAuth = () => {
   return user? (
     <Outlet />
   ) : (
-    <Navigate to="/reqLogin" state={{ from: location }} replace />
+    <Navigate to="/login" state={{ from: location }} replace />
   );
 };
 
