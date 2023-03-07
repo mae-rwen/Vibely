4da99468bfa5a5abf071e7370ee198fb34d3b15c
Agata Thrams <agata.thrams@gmail.com>
2023-03-07 13:17:56 +0100

    now?

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,9 @@ const app = express();
 const port = process.env.PORT || 3001;
 app.use(express.json({ limit: "10mb" }));
 
-app.use(express.static(path.resolve(__dirname, "../frontend", "build")));
+app.use(express.static(path.resolve(__dirname, "..", "frontend", "build")));
+
+
 
 // app.use(express.static("/uploads"));
 app.use(
@@ -40,9 +42,9 @@ app.use("/categories", categoriesRouter);
 app.use("/booking", bookingRouter);
 
 app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../frontend", "build", "index.html"));
+  res.sendFile(path.join(__dirname, "..", "frontend", "build", "index.html"));
  });
- 
+
 
 
 app.use(errorHandler);
